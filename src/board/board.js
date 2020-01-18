13c38b61900d552a86aa2a3214632407e106e9ed
Yura Manucharyan <mnuch95@mail.ru>
2020-01-18 12:45:07 +0400

    Implement collect combinations logic

--- src/board/board.js
+++ src/board/board.js
@@ -5,31 +5,37 @@ import { Cell } from "./cell";
 export class Board extends Phaser.GameObjects.Container {
   constructor(scene) {
     super(scene);
-    this._cells = [];
-    this._selectedBall = null;
-    this._buildBoard();
-    this._makeRandomBalls();
-  }
 
-  getCellByBall(ball) {
-    for (let i = 0; i < this._cells.length; i++) {
-      const cols = this._cells[i];
-      for (let j = 0; j < cols.length; j++) {
-        const cell = cols[j];
-        if (cell.ball === ball) {
-          return cell;
-        }
-      }
-    }
+    this._cells = [];
+    this._combinations = [];
+    this._selectedBall = null;
+
+    this._buildBoard();
+    this._makeBalls();
   }
 
   getRandomEmptyCell() {
     const rndI = Math.floor(Math.random() * BOARD_DIMENSIONS.width);
     const rndJ = Math.floor(Math.random() * BOARD_DIMENSIONS.height);
-    if (!this._cells[rndI][rndJ].isEmpty) {
+    const rndCell = this._cells[rndI][rndJ];
+
+    if (!rndCell.isEmpty) {
       return this.getRandomEmptyCell();
     }
-    return this._cells[rndI][rndJ];
+
+    return rndCell;
+  }
+
+  getCellByBall(ball) {
+    for (let i = 0; i < this._cells.length; i++) {
+      const columns = this._cells[i];
+      for (let j = 0; j < columns.length; j++) {
+        const cell = columns[j];
+        if (cell.ball === ball) {
+          return cell;
+        }
+      }
+    }
   }
 
   _buildBoard() {
@@ -40,73 +46,35 @@ export class Board extends Phaser.GameObjects.Container {
         const cell = new Cell(this.scene, row, col);
         this.add(cell);
         column.push(cell);
-
         const { width, height } = cell;
         cell.setPosition(col * width + width / 2, row * height + height / 2);
         cell.on("onCellClick", this._onCellClicked, this);
       }
+
       this._cells.push(column);
     }
   }
 
-  _makeRandomBalls() {
+  _makeBalls() {
     for (let i = 0; i < 3; i++) {
       const ball = this._generateRandomBall();
       const cell = this.getRandomEmptyCell();
+
       cell.addBall(ball);
     }
   }
 
   _generateRandomBall() {
     const type = Math.floor(Math.random() * 4 + 1);
-
     const ball = new Ball(this.scene, type);
+
     return ball;
-  }
-
-  _checkForCombination() {
-    for (let i = 0; i < this._cells.length; i++) {
-      const cols = this._cells[i];
-      for (let j = 0; j < cols.length; j++) {
-        const cell = cols[j];
-        if (!cell.isEmpty) {
-          const combination = this._checkHorizontalCombination(
-            cell.ball,
-            i,
-            j,
-            []
-          );
-        }
-      }
-    }
-  }
-
-  _checkHorizontalCombination(ball, col, row, combination) {
-    if (col + 1 >= this._cells.length) {
-      return combination;
-    }
-    const cell = this._cells[col + 1][row];
-    if (!cell.ball || cell.ball.type !== ball.type) {
-      return combination;
-    }
-    combination.push(ball);
-    return this._checkHorizontalCombination(ball, col + 1, row, combination);
-  }
-
-  _moveBall(newCell) {
-    const prevCell = this.getCellByBall(this._selectedBall);
-    prevCell.removeBall();
-    newCell.addBall(this._selectedBall);
-    this._selectedBall.deselectBall();
-    this._selectedBall = null;
-    this._checkForCombination();
-    this._makeRandomBalls();
-    this._checkForCombination();
   }
 
   _onCellClicked(col, row) {
     const cell = this._cells[col][row];
     const { isEmpty } = cell;
+
     if (isEmpty) {
       if (this._selectedBall) {
         this._moveBall(cell);
@@ -118,5 +86,103 @@ export class Board extends Phaser.GameObjects.Container {
       this._selectedBall = cell.ball;
       this._selectedBall.selectBall();
     }
+  }
+
+  _moveBall(newCell) {
+    const prevCell = this.getCellByBall(this._selectedBall);
+    prevCell.removeBall();
+    newCell.addBall(this._selectedBall);
+    this._selectedBall.deselectBall();
+    this._selectedBall = null;
+    this._checkForCombination();
+    if (this._combinations.length === 0) {
+      this._makeBalls();
+      this._checkForCombination();
+    }
+  }
+
+  _checkForCombination() {
+    this._combinations.length = 0;
+
+    for (let i = 0; i < this._cells.length; i++) {
+      const column = this._cells[i];
+      for (let j = 0; j < column.length; j++) {
+        const cell = column[j];
+        if (!cell.isEmpty) {
+          const hComb = this._getHorizontalCombination(cell.ball, i, j, [
+            cell.ball
+          ]);
+          const vComb = this._getVerticalCombination(cell.ball, i, j, [
+            cell.ball
+          ]);
+
+          if (hComb.length >= 5) this._combinations.push(hComb);
+          if (vComb.length >= 5) this._combinations.push(vComb);
+        }
+      }
+    }
+
+    this._collectCombinations();
+  }
+
+  _getHorizontalCombination(ball, col, row, combination) {
+    if (col + 1 >= this._cells.length) {
+      return combination;
+    }
+
+    const cell = this._cells[col + 1][row];
+
+    if (
+      !cell.ball ||
+      cell.ball.type !== ball.type ||
+      this._alreadyConsistInCombination(ball)
+    ) {
+      return combination;
+    }
+
+    combination.push(cell.ball);
+
+    return this._getHorizontalCombination(cell.ball, col + 1, row, combination);
+  }
+
+  _getVerticalCombination(ball, col, row, combination) {
+    if (row + 1 >= this._cells[col].length) {
+      return combination;
+    }
+
+    const cell = this._cells[col][row + 1];
+
+    if (
+      !cell.ball ||
+      cell.ball.type !== ball.type ||
+      this._alreadyConsistInCombination(ball)
+    ) {
+      return combination;
+    }
+
+    combination.push(cell.ball);
+
+    return this._getVerticalCombination(cell.ball, col, row + 1, combination);
+  }
+
+  _alreadyConsistInCombination(ball) {
+    for (let i = 0; i < this._combinations.length; i++) {
+      const combination = this._combinations[i];
+      for (let j = 0; j < combination.length; j++) {
+        if (combination[j].uuid === ball.uuid) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  _collectCombinations() {
+    this._combinations.forEach(combination => {
+      combination.forEach(ball => {
+        const cell = this.getCellByBall(ball);
+        cell.removeBall();
+      });
+    });
   }
 }
